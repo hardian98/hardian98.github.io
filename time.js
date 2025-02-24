e0b2ad1c85fb2feee41b32c27bac52c0c058c807
hardian98 <143332250+hardian98@users.noreply.github.com>
2025-02-24 20:41:48 +0700

    Add files via upload

--- time.js
+++ time.js
@@ -46,6 +46,71 @@
 		},1e3)
 	}
 	
+	// Sambutan
+	day = new Date()
+	hr = day.getHours()
+	
+	if ((hr==1) || (hr==2))
+	document.getElementsById("ucapan")[0].innerHTML = "Selamat Pagi.<br/>Kebangun, ya?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Selamat Pagi.<br/>Kebangun, ya?";
+
+	if (hr==3)
+	document.getElementsById("ucapan")[0].innerHTML = "Tidur lagi, yuk.<br/>Tapi jangan lupa untuk charge ponselmu, ya.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Tidur lagi, yuk.<br/>Tapi jangan lupa untuk charge ponselmu, ya.";
+
+	if (hr==4)
+	document.getElementsById("ucapan")[0].innerHTML = "Mau lanjut tidur apa belum tidur, nih?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Mau lanjut tidur apa belum tidur, nih?";
+
+	if (hr==5)
+	document.getElementsById("ucapan")[0].innerHTML = "Jam 5 Pagi.<br/>Tidak lupa untuk mengisi daya ponselmu, kan?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Jam 5 Pagi.<br/>Tidak lupa untuk mengisi daya ponselmu, kan?";
+
+	if (hr==6)
+	document.getElementsById("ucapan")[0].innerHTML = "Saya harap kamu tidak lupa me-charge ponselmu semalam.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Saya harap kamu tidak lupa me-charge ponselmu semalam.";
+
+	if ((hr==7) || (hr==8))
+	document.getElementsById("ucapan")[0].innerHTML = "Selamat Pagi.<br/>Selamat Beraktifitas.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Selamat Pagi.<br/>Selamat Beraktifitas.";
+
+	if ((hr==9) || (hr ==10) || (hr== 11))
+	document.getElementsById("ucapan")[0].innerHTML = "Selamat Bekerja.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Selamat Bekerja.";
+
+	if (hr==12)
+	document.getElementsById("ucapan")[0].innerHTML = "Waktunya makan siang.<br/>Bagaimana kondisi baterai ponselmu siang ini?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Waktunya makan siang.<br/>Bagaimana kondisi baterai ponselmu siang ini?";
+
+	if ((hr==13) || (hr==14))
+	document.getElementsById("ucapan")[0].innerHTML = "Selamat Siang.<br/>Selalu perhatikan suhu ponselmu.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Selamat Siang.<br/>Selalu perhatikan suhu ponselmu.";
+
+	if ((hr==15) || (hr==16))
+	document.getElementsById("ucapan")[0].innerHTML = "Masih baik-baik saja?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Masih baik-baik saja?";
+	
+	if (hr==17)
+	document.getElementsById("ucapan")[0].innerHTML = "Butuh pengisian?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Need charging?";
+	
+	if (hr==18)
+	document.getElementsById("ucapan")[0].innerHTML = "Hari yang bagus!<br/>Berapa % daya baterai ponselmu?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Hari yang bagus!<br/>Berapa % daya baterai ponselmu?";
+
+	if ((hr==19) || (hr==20))
+	document.getElementsById("ucapan")[0].innerHTML = "Selamat Beristirahat.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Selamat Beristirahat.";
+
+	if ((hr==21) || (hr==22))
+	document.getElementsById("ucapan")[0].innerHTML = "Waktunya Tidur?<br/>Atau masih menikmati santaimu?";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Waktunya Tidur?<br/>Atau masih menikmati santaimu?";
+
+	if ((hr==23) || (hr==0))
+	document.getElementsById("ucapan")[0].innerHTML = "Saatnya Tidur.<br/>Jangan lupa istirahatkan dan charge ponselmu.";
+	// document.getElementsByClassName("ucapanid")[0].innerHTML = "Saatnya Tidur.<br/>Jangan lupa istirahatkan dan charge ponselmu.";
+	// End -->
+	
 	<!-- TGL & JAM -->
 	var months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 	var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum&#39;at", "Sabtu"];
